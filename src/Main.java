3db4696c2ffed60b2dfa5ddf2853a3e033c6fe65
Agustin Moldavsky <agusting.moldavsky64gmail.com>
2024-06-07 01:28:36 -0300

    Inicio

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner =new Scanner(System.in);
+
+        String palabraSecreta = "inteligencia";
+        int intentosMaximos = 10;
+        int intentos = 0;
+        boolean palabraAdivinada = false;
+
+
+    }
+}
