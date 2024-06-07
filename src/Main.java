048d4c634c47d71c1c83b7f06484d08b03ac4d61
Agustin Moldavsky <agusting.moldavsky64gmail.com>
2024-06-07 02:02:15 -0300

    Juego terminado

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,44 @@ public class Main {
         boolean palabraAdivinada = false;
 
 
+        char[] letrasAdivinadas = new char[palabraSecreta.length()];
+
+        for (int i = 0; i < letrasAdivinadas.length; i++) {
+            letrasAdivinadas[i] = '_';
+            System.out.print(letrasAdivinadas[i]);
+        }
+
+        while (!palabraAdivinada && intentos < intentosMaximos){
+
+            System.out.println("Palabra a adivinar: " + String.valueOf(letrasAdivinadas) + " (" + palabraSecreta.length() + " letras)");
+
+            System.out.println("Introduce una letra, por favor");
+            char letra = Character.toLowerCase(scanner.next().charAt(0));
+
+            boolean letraCorrecta = false;
+            for (int i = 0; i < palabraSecreta.length() ; i++) {
+
+                if(palabraSecreta.charAt(i)== letra){
+                        letrasAdivinadas[i] = letra;
+                        letraCorrecta = true;
+
+                }
+        }
+            if(!letraCorrecta){
+                intentos++;
+                System.out.println("Incorrecto! Te quedan: " + (intentosMaximos - intentos) + " intentos." );
+
+            }
+
+            if(String.valueOf(letrasAdivinadas).equals(palabraSecreta)){
+                    palabraAdivinada = true;
+                    System.out.println("Felicidades! Has adivinado la palabra secreta: " + palabraSecreta);
+            }
+        }
+
+        if(!palabraAdivinada){
+            System.out.println("Que pena te has quedado sin intentos! Game over");
+        }
+        scanner.close();
     }
 }
